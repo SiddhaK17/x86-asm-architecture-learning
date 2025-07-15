c4618a520200809365b7972905396a1716705d6c
SiddhaK <siddhakad17@gmail.com>
2025-07-15 10:13:55 +0530

    feat(char_str_list.s): load and inspect string data using registers and memory access

--- 01-basics-of-x86-architecture/04-characters-strings-and-lists/char_str_list.s
+++ 01-basics-of-x86-architecture/04-characters-strings-and-lists/char_str_list.s
@@ -1,1 +1,15 @@
+section .data
+      ; char DB 'A'
+      ; list DB 1,2,3,4,0
+        string1 DB "ABA",0
+        string2 DB "CDE",0
 
+section .text
+global _start
+
+_start:
+      ; MOV bl,[char]
+      ; MOV bl,[list]
+        MOV bl,[string1]
+        MOV eax,1
+        INT 80h
